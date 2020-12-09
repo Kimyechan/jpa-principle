a25b6f26457608628b0341fb0cc8d639478777cc
Kimyechan <vlvkcjswo7@naver.com>
2020-12-09 16:12:50 +0900

    Feat: Domain Design

--- src/main/java/com/hellojpa/Member.java
+++ src/main/java/com/hellojpa/Member.java
@@ -4,8 +4,13 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 1)
 public class Member {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @Column(name = "name")
