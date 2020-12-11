e502495b7198e97c3f02c33b20b1e6785f892abc
Kimyechan <vlvkcjswo7@naver.com>
2020-12-11 15:55:51 +0900

    Feat: Entity Inheritance

---
+++ src/main/java/com/hellojpa/Book.java
@@ -0,0 +1,9 @@
+package com.hellojpa;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Book extends Item{
+    private String author;
+    private String isbn;
+}
---
+++ src/main/java/com/hellojpa/Item.java
@@ -0,0 +1,15 @@
+package com.hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+//@Inheritance(strategy = InheritanceType.JOINED)
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@DiscriminatorColumn // DTYPE
+public class Item {
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+    private int price;
+}
