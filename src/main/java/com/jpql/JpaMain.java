2da54f74f535542bc3042cecbe91d6a760e9bab1
Kimyechan <vlvkcjswo7@naver.com>
2020-12-13 01:30:07 +0900

    Feat: Paging

--- src/main/java/com/jpql/JpaMain.java
+++ src/main/java/com/jpql/JpaMain.java
@@ -18,40 +18,28 @@ public class JpaMain {
         try {
             Team team = new Team();
             team.setName("team1");
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setAge(10);
+                member.setUsername("member" + i);
+                member.setTeam(team);
 
-            Member member = new Member();
-            member.setAge(10);
-            member.setUsername("member1");
-            member.setTeam(team);
-
-            em.persist(member);
+                em.persist(member);
+            }
             em.persist(team);
 
             em.flush();
             em.clear();
 
-            List<Member> members = em.createQuery("select m from Member m join m.team t", Member.class)
-                                        .getResultList();
-
-            Member member1 = members.get(0);
-            member1.setAge(20);
-
-            em.createQuery("select o.address from  Order o", Address.class)
+            List<Member> result = em.createQuery("select m from Member m order by m.id desc ", Member.class)
+                    .setFirstResult(10)
+                    .setMaxResults(20)
                     .getResultList();
 
-            List result = em.createQuery("select distinct m.username, m.age from  Member m")
-                    .getResultList();
-
-            Object o = result.get(0);
-            Object[] member2 = (Object[])o;
-            System.out.println("username = " + member2[0]);
-            System.out.println("age = " + member2[1]);
-
-            List<Object[]> result1 = em.createQuery("select distinct m.username, m.age from  Member m")
-                    .getResultList();
-
-            List<MemberDTO> result2 = em.createQuery("select new com.jpql.MemberDTO(m.username, m.age) from  Member m", MemberDTO.class)
-                    .getResultList();
+            System.out.println("result.size = " + result.size());
+            for ( Member member1 : result) {
+                System.out.println("member = " + member1);
+            }
 
 
 
