0fffec996f785cfc899c5722e8d6dcb169f06bb3
Kimyechan <vlvkcjswo7@naver.com>
2020-12-12 13:10:38 +0900

    Feat: Cascade And Orphan(고아객체)

--- src/main/java/com/hellojpa/JpaMain.java
+++ src/main/java/com/hellojpa/JpaMain.java
@@ -18,55 +18,22 @@ public class JpaMain {
 
         tx.begin();
         try {
-            /*Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team);
-            em.persist(member1);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            em.flush();
-            em.clear();
-
-            Member m = em.find(Member.class, member1.getId());
-
-            System.out.println(m.getTeam().getClass());
-
-            System.out.println("==============");
-            System.out.println(m.getTeam().getName());
-            System.out.println("==============");*/
-
-            Team team1 = new Team();
-            team1.setName("teamA");
-            em.persist(team1);
-
-            Team team2= new Team();
-            team2.setName("teamA");
-            em.persist(team2);
-
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team1);
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setTeam(team2);
-            em.persist(member2);
+            em.persist(parent);
+//            em.persist(child1);
+//            em.persist(child2);
 
             em.flush();
             em.clear();
 
-            // N + 1 Problem
-            // SQL : select * from member;
-            // SQL : select * from Team where TEAM_ID = xxx
-//            List<Member> members = em.createQuery("select m from Member m", Member.class)
-//                    .getResultList();
-
-            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class)
-                    .getResultList();
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
 
             tx.commit();
         } catch (Exception e) {
