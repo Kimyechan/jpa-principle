230609f46d78938e223dfc78a9889048094f0749
Kimyechan <vlvkcjswo7@naver.com>
2020-12-13 15:13:49 +0900

    Feat: Fetch Join

--- src/main/java/com/jpql/JpaMain.java
+++ src/main/java/com/jpql/JpaMain.java
@@ -17,35 +17,51 @@ public class JpaMain {
 
         tx.begin();
         try {
-            Team team = new Team();
-            team.setName("team1");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setAge(10);
-            member.setUsername("member1");
-            member.setType(MemberType.ADMIN);
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
 
-            member.changeTeam(team);
+            Member member1 = new Member();
+            member1.setAge(10);
+            member1.setUsername("member1");
+            member1.setTeam(teamA);
+            em.persist(member1);
 
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setAge(10);
+            member2.setUsername("member2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setAge(10);
+            member3.setUsername("member3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-//            String query = "select m.username from Member m";
-//            String query = "select m.team.name from Member m";
-//            String query = "select t.members.size from Team t";
-            String query = "select m.username from Team t join t.members m";
-//            List<String> result = em.createQuery(query, String.class)
-//                    .getResultList();
-            String result = em.createQuery(query, String.class)
-                    .getSingleResult();
+//            String query = "select m From Member m"; // -> N + 1
+//            String query = "select t From Team t join fetch t.members"; // 중복된 데이터 발생한다
+//            String query = "select distinct t From Team t join fetch t.members"; // 중복제거
+//            String query = "select m From Member m join m.team "; // 일반 조인 member data만 가지고 온다. team이 로딩되지 않는다
+            String query = "select m From Member m join fetch m.team";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
 
-//            for (String s: result) {
-//                System.out.println("s = " + s);
-//            }
+            for (Member member : result) {
+                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+                //회원1, 팀A(SQL)
+                //회원2, 팀A(1차 캐시)
+                //회원3, 팀B(SQL)
 
+                //회원 100명 -> N + 1
+            }
             tx.commit();
         } catch (Exception e) {
             e.printStackTrace();
